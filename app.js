bac5f5ffa92946f4a5396d652d1a523e5e3423d0
jpdhernandez <jpdhernandez@gmail.com>
2017-02-04 07:29:44 -0500

    Add  error handling

--- app.js
+++ app.js
@@ -14,15 +14,21 @@ const argv = yargs
     .alias("help", "h")
     .argv;
 
-const address = encodeURIComponent(argv[1]);
+const address = encodeURIComponent(argv.address);
 
 request({
     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(
-        `Lat: ${body.results[0].geometry.location.lat}\n` +
-        `Lng: ${body.results[0].geometry.location.lng}`
-    );
+    if (error) {
+        return console.error("Unable to connect to Google servers");
+    } else if (body.status === "ZERO_RESULTS") {
+        return console.error(`Unable to find "${address}" address`);
+    } else if (body.status === "OK") {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(
+            `Lat: ${body.results[0].geometry.location.lat}\n` +
+            `Lng: ${body.results[0].geometry.location.lng}`
+        );
+    }
 });
