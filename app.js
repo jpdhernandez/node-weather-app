0dc228043cd9c24d9e928aa6627e179b6681b677
jpdhernandez <jpdhernandez@gmail.com>
2017-02-04 07:03:44 -0500

    Parse body to grab address and geolocation

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
     url: "http://maps.googleapis.com/maps/api/geocode/json?address=cn%20tower%20toronto",
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}\n` +
+        `Lng: ${body.results[0].geometry.location.lng}`
+    );
 });
