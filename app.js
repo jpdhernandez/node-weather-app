7debfccd9045f6efa425555abdd223200e8c234f
jpdhernandez <jpdhernandez@gmail.com>
2017-02-04 07:16:53 -0500

    Add functionality to make dynamic address lookup from user input

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: "address",
+            describe: "Address to fetch weather for",
+            string: true
+        }
+    })
+    .help()
+    .alias("help", "h")
+    .argv;
+
+const address = encodeURIComponent(argv[1]);
 
 request({
-    url: "http://maps.googleapis.com/maps/api/geocode/json?address=cn%20tower%20toronto",
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}\n` +
+    console.log(
+        `Lat: ${body.results[0].geometry.location.lat}\n` +
         `Lng: ${body.results[0].geometry.location.lng}`
     );
 });
